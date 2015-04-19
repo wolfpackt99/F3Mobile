2b69d2d6c23d67ede20170adaa1a928cdb1b8f00
Trent <trent.jones>
2015-04-19 16:50:09 -0400

    Fix broken tests.

--- F3Mobile.Tests/Controllers/HomeControllerTest.cs
+++ F3Mobile.Tests/Controllers/HomeControllerTest.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Mvc;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using F3Mobile;
+﻿using System.Web.Mvc;
 using F3Mobile.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace F3Mobile.Tests.Controllers
 {
@@ -16,10 +11,10 @@ namespace F3Mobile.Tests.Controllers
         public void Index()
         {
             // Arrange
-            HomeController controller = new HomeController();
+            var controller = new HomeController();
 
             // Act
-            ViewResult result = controller.Index() as ViewResult;
+            var result = controller.Index() as ViewResult;
 
             // Assert
             Assert.IsNotNull(result);
@@ -29,23 +24,23 @@ namespace F3Mobile.Tests.Controllers
         public void About()
         {
             // Arrange
-            HomeController controller = new HomeController();
+            var controller = new HomeController();
 
             // Act
-            ViewResult result = controller.About() as ViewResult;
+            var result = controller.FNG() as ViewResult;
 
             // Assert
-            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+            Assert.AreEqual("FNG Signup", result.ViewBag.Message);
         }
 
         [TestMethod]
         public void Contact()
         {
             // Arrange
-            HomeController controller = new HomeController();
+            var controller = new HomeController();
 
             // Act
-            ViewResult result = controller.Contact() as ViewResult;
+            var result = controller.Contact() as ViewResult;
 
             // Assert
             Assert.IsNotNull(result);
