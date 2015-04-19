f4423e3d06d57dcb887faedbf793d6e28b9b92d3
Trent <trent.jones>
2015-04-19 10:00:08 -0400

    initial

---
+++ F3Mobile/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(F3Mobile.Startup))]
+namespace F3Mobile
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
