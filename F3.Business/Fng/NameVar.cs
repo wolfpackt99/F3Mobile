3f6cdd085c79fe040d72c4b2805365d67c6f0be0
Trent <trent.jones>
2015-04-30 11:30:17 -0400

    Capture First and Last name

---
+++ F3.Business/Fng/NameVar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MailChimp.Lists;
+
+namespace F3.Business.Fng
+{
+    [System.Runtime.Serialization.DataContract]
+    public class NameVar : MergeVar
+    {
+        [System.Runtime.Serialization.DataMember(Name = "FNAME")]
+        public string FirstName { get; set; }
+        [System.Runtime.Serialization.DataMember(Name = "LNAME")]
+        public string LastName { get; set; }
+    }
+}
--- F3.Business/MailChimpBusiness.cs
+++ F3.Business/MailChimpBusiness.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using F3.Business.Fng;
 using MailChimp;
 using MailChimp.Helper;
 
@@ -19,7 +20,12 @@ namespace F3.Business
             {
                 Email = contact.Email
             };
-            var result = mc.Subscribe(ConfigurationManager.AppSettings.Get("F3List"), email, doubleOptIn: false,
+            var name = new NameVar
+            {
+                FirstName = contact.FirstName,
+                LastName = contact.LastName
+            };
+            var result = mc.Subscribe(ConfigurationManager.AppSettings.Get("F3List"), email, name, doubleOptIn: false,
                 sendWelcome: true);
             return true;
         }
