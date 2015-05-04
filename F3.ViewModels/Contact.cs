eefd53b83f5668c005662c7b6b3f5358d363d601
Trent <trent.jones>
2015-05-04 14:28:43 -0400

    fix dropdowns.

--- F3.ViewModels/Contact.cs
+++ F3.ViewModels/Contact.cs
@@ -5,18 +5,24 @@ namespace F3.ViewModels
 {
     public class Contact
     {
+        [Required]
         public string Id { get; set; }
         [Display(ResourceType = typeof(ResourceStrings), Name="FirstName")]
         public string FirstName { get; set; }
+
+        [Required]
         [Display(ResourceType = typeof(ResourceStrings), Name = "LastName")]
         public string LastName { get; set; }
+
         [Required]
         [Display(ResourceType = typeof(ResourceStrings), Name = "Email")]
         [EmailAddress]
         public string Email { get; set; }
 
+        [Required]
         [Display(ResourceType = typeof(ResourceStrings), Name = "F3Name")]
         public string F3Name { get; set; }
+
         [Display(ResourceType = typeof(ResourceStrings), Name = "Workout")]
         public string Workout { get; set; }
         
