19d651be0ab3cb60b29d41de845f21e4c3d2022b
Trent <trent.jones>
2015-09-09 18:56:04 -0400

    fix build.

--- F3.Business.Tests/CalenderFixture.cs
+++ F3.Business.Tests/CalenderFixture.cs
@@ -38,27 +38,27 @@ namespace F3.Business.Tests
             sut.Items.Should().HaveCount(c => c > 0);
         }
 
-        //[TestMethod]
-        public async Task GetAllEventsForWeek()
-        {
-            var cb = new CalendarBusiness
-            {
-                CacheService = new CacheService()
-            };
-            var sut = await cb.GetAllEvents(false);
-            sut.Should().NotBeEmpty();
-            sut.First().Summary.ShouldBeEquivalentTo("Anvil");
-        }
-        //[TestMethod]
-        public async Task GetAllEventsForAll()
-        {
-            var cb = new CalendarBusiness
-            {
-                CacheService = new CacheService()
-            };
-            var sut = await cb.GetAllEvents();
-            sut.Should().NotBeEmpty();
-            sut.First().Summary.ShouldBeEquivalentTo("Anvil");
-        }
+        ////[TestMethod]
+        //public async Task GetAllEventsForWeek()
+        //{
+        //    var cb = new CalendarBusiness
+        //    {
+        //        CacheService = new CacheService()
+        //    };
+        //    var sut = await cb.GetAllEvents(false);
+        //    sut.Should().NotBeEmpty();
+        //    sut.First().Summary.ShouldBeEquivalentTo("Anvil");
+        //}
+        ////[TestMethod]
+        //public async Task GetAllEventsForAll()
+        //{
+        //    var cb = new CalendarBusiness
+        //    {
+        //        CacheService = new CacheService()
+        //    };
+        //    var sut = await cb.GetAllEvents();
+        //    sut.Should().NotBeEmpty();
+        //    sut.First().Summary.ShouldBeEquivalentTo("Anvil");
+        //}
     }
 }
